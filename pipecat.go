6737acd0828362cdbb5b7acd914c3fc83507eac3
Lukas Martinelli <me@lukasmartinelli.ch>
2015-08-14 14:53:34 +0200

    Basic refactoring

--- pipecat.go
+++ pipecat.go
@@ -18,6 +18,26 @@ func failOnError(err error, msg string) {
 	}
 }
 
+func prepare(amqpUri string, queueName string) (*amqp.Connection, *amqp.Channel) {
+	conn, err := amqp.Dial(amqpUri)
+	failOnError(err, "Failed to connect to AMPQ broker")
+
+	channel, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+
+	_, err = channel.QueueDeclare(
+		queueName, // name
+		true,      // durable
+		false,     // delete when unused
+		false,     // exclusive
+		false,     // no-wait
+		nil,       // arguments
+	)
+	failOnError(err, "Failed to declare queue")
+
+	return conn, channel
+}
+
 func main() {
 	app := cli.NewApp()
 	app.Name = "pipecat"
@@ -51,23 +71,9 @@ func main() {
 					os.Exit(1)
 				}
 
-				conn, err := amqp.Dial(c.String("amqpUri"))
-				failOnError(err, "Failed to connect to AMPQ broker")
+				conn, channel := prepare(c.String("amqpUri"), queueName)
 				defer conn.Close()
-
-				channel, err := conn.Channel()
-				failOnError(err, "Failed to open a channel")
 				defer channel.Close()
-
-				q, err := channel.QueueDeclare(
-					queueName, // name
-					true,      // durable
-					false,     // delete when unused
-					false,     // exclusive
-					false,     // no-wait
-					nil,       // arguments
-				)
-				failOnError(err, "Failed to declare a queue")
 
 				stdack, err := os.OpenFile(c.String("fack"), os.O_APPEND, 0660)
 				failOnError(err, "Could not open ack file")
@@ -77,10 +83,10 @@ func main() {
 				for scanner.Scan() {
 					line := scanner.Text()
 					err = channel.Publish(
-						"",     // exchange
-						q.Name, // routing key
-						false,  // mandatory
-						false,  // immediate
+						"",        // exchange
+						queueName, // routing key
+						false,     // mandatory
+						false,     // immediate
 						amqp.Publishing{
 							ContentType: "text/plain",
 							Body:        []byte(line),
@@ -108,12 +114,8 @@ func main() {
 					os.Exit(1)
 				}
 
-				conn, err := amqp.Dial(c.String("amqpUri"))
-				failOnError(err, "Failed to connect to AMPQ broker")
+				conn, channel := prepare(c.String("amqpUri"), queueName)
 				defer conn.Close()
-
-				channel, err := conn.Channel()
-				failOnError(err, "Failed to open a channel")
 				defer channel.Close()
 
 				var mutex sync.Mutex
